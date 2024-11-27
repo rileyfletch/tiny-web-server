63cc4409779ca7ec6137f87a9b898015ee2f7308
Riley Fletcher <89424122+nrfletcher@users.noreply.github.com>
2024-11-26 23:48:33 -0500

    working?

--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "server/other.h"
-#include "utils/test.h"
-
-int main() {
-    std::cout << func1() << std::endl;
-    std::cout << func2() << std::endl;
-    return 1;
-}
